d70d1b1bf969d359e7972b2693f0ac4527f0828a
Harivan-T <harivantahir@gmail.com>
2024-03-11 23:35:03 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers =[
+    "Alan",
+   "kurdistan",
+   "birhat","amad","Kovan"
+];
+
+function findMatching(drivers, name) {
+   return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+      
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
